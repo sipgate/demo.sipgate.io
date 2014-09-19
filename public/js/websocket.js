0196261bde384a66af9612490194836983cad0e1
Benjamin Kluck <kluck@sipgate.de>
2014-09-19 12:08:26 +0200

    Add comments to websocket.js

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 	// Append numbers to phone number table
 	function insertRow(from, to) {
-		var date = new Date();
-
 		$("#phone-number-table").find('tbody')
 			.prepend($('<tr>')
 				.append($('<td>')
@@ -17,8 +15,10 @@ $(document).ready(function () {
 			);
 	}
 
-	// Insert one dummy row
-	insertRow("0211 12345XXX", "01579 1234567");
+	// Insert one dummy row after three seconds
+	window.setTimeout(function(){
+		insertRow("0211 12345XXX", "01579 1234XXX");
+	}, 3000);
 
 	// Connect socket.io client
 	var sock = io.connect();
@@ -27,10 +27,13 @@ $(document).ready(function () {
 	sock.on('new call', function (data) {
 		console.log(data);
 
+		// Update number box
 		$('#number').text(data.from);
 
+		// Append number to phone number table
 		insertRow(data.from, data.to);
 
+		// Animate phone image
 		$('#phone').addClass("animated tada");
 		window.setTimeout(function(){
 			$('#phone').removeClass("animated tada");
