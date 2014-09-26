f38520d73d9fc1341bc932d7bc5414111fa17e1a
Benjamin Kluck <kluck@sipgate.de>
2014-09-26 14:52:09 +0200

    Support anonymous calls

--- server.js
+++ server.js
@@ -20,30 +20,38 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve 'index.html'
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
 	res.render('index');
 });
 
 // Process API POST requests
-app.post("/", function (request, response) {
+app.post("/", function(request, response) {
+
 	// Send response to API
 	response.send("So Long, and Thanks for All the Fish!");
 
 	// Parse and format numbers
-	var fromNumber = phoneUtil.parse("+" + request.body.from, "DE");
-	var toNumber = phoneUtil.parse("+" + request.body.to, "DE");
+	var formatNumber = function(number) {
+		if (number.match("anonymous")) {
+			return "Anonym";
+		}
 
-	var format = phone.PhoneNumberFormat.INTERNATIONAL;
-	if (fromNumber.getCountryCode() == 49) {
-		format = phone.PhoneNumberFormat.NATIONAL;
+		var parsedNumber = phoneUtil.parse("+" + number, "DE");
+
+		var format = phone.PhoneNumberFormat.INTERNATIONAL;
+		if (parsedNumber.getCountryCode() == 49) {
+			format = phone.PhoneNumberFormat.NATIONAL;
+		}
+
+		return phoneUtil.format(parsedNumber, format).replace(/...$/, 'xxx');
 	}
 
-	var from = phoneUtil.format(fromNumber, format);
-	var to = phoneUtil.format(toNumber, phone.PhoneNumberFormat.NATIONAL);
+	var from = formatNumber(request.body.from);
+	var to = formatNumber(request.body.to);
 
-	// Send anonymous 'from' and 'to' to all connected socket.io clients
+	// Send 'from' and 'to' to all connected socket.io clients
 	io.sockets.emit('new call', {
-		from: from.replace(/...$/, 'xxx'),
-		to: to.replace(/...$/, 'xxx'),
+		from: from,
+		to: to,
 	});
 });
